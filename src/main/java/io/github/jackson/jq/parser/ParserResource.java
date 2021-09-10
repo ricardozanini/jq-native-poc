6958017c1f9e89812e94e5055cd7a6ed6a86db3e
Ricardo Zanini <zanini@redhat.com>
2021-09-10 13:09:14 -0300

    Eager load Scope bean
    Signed-off-by: Ricardo Zanini <zanini@redhat.com>

--- src/main/java/io/github/jackson/jq/parser/ParserResource.java
+++ src/main/java/io/github/jackson/jq/parser/ParserResource.java
@@ -3,6 +3,7 @@ package io.github.jackson.jq.parser;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -10,7 +11,6 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import net.thisptr.jackson.jq.BuiltinFunctionLoader;
 import net.thisptr.jackson.jq.JsonQuery;
 import net.thisptr.jackson.jq.Scope;
 import net.thisptr.jackson.jq.Versions;
@@ -21,12 +21,8 @@ import net.thisptr.jackson.jq.exception.JsonQueryException;
 @Consumes(MediaType.APPLICATION_JSON)
 public class ParserResource {
 
-    private final Scope rootScope;
-
-    public ParserResource() {
-        this.rootScope = Scope.newEmptyScope();
-        BuiltinFunctionLoader.getInstance().loadFunctions(Versions.JQ_1_6, this.rootScope);
-    }
+    @Inject
+    Scope rootScope;
 
     @POST
     public List<JsonNode> parse(Document document) throws JsonQueryException {
