33466e8355e52a2063cc7dc89b2ca2e127f7feee
Ricardo Zanini <zanini@redhat.com>
2021-09-10 15:48:02 -0300

    Adding timestamp to the app
    Signed-off-by: Ricardo Zanini <zanini@redhat.com>

--- src/main/java/io/github/jackson/jq/parser/ParserResource.java
+++ src/main/java/io/github/jackson/jq/parser/ParserResource.java
@@ -1,8 +1,11 @@
 package io.github.jackson.jq.parser;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
+import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
@@ -11,9 +14,8 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import net.thisptr.jackson.jq.BuiltinFunctionLoader;
+import io.quarkus.runtime.StartupEvent;
 import net.thisptr.jackson.jq.JsonQuery;
-import net.thisptr.jackson.jq.Scope;
 import net.thisptr.jackson.jq.Versions;
 import net.thisptr.jackson.jq.exception.JsonQueryException;
 import net.thisptr.jackson.jq.quarkus.JacksonJqQuarkusScope;
@@ -32,10 +34,15 @@ public class ParserResource {
 
     @POST
     public List<JsonNode> parse(Document document) throws JsonQueryException {
+        System.out.println(new SimpleDateFormat("HH:mm:ss.SSS").format(new java.util.Date(System.currentTimeMillis())));
         final JsonQuery query = JsonQuery.compile(document.expression, Versions.JQ_1_6);
         List<JsonNode> out = new ArrayList<>();
         query.apply(this.rootScope, document.document, out::add);
         return out;
+    }
+
+    void onStart(@Observes StartupEvent startup) {
+        System.out.println(new SimpleDateFormat("HH:mm:ss.SSS").format(new Date()));
     }
 
     public static class Document {
